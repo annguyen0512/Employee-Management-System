bc1b6aa58ddba2fd3e131730298f0e4269cc5164
ITCSIU21161 <118712843+annguyen0512@users.noreply.github.com>
2024-03-21 23:57:59 +0700

    JWT generate and store inside cookie

--- Employee MS/Server/index.js
+++ Employee MS/Server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
+import {adminRouter} from "./Routes/AdminRoute.js";
 
 const app = express();
 const port = 3000;
+app.use(cors({
+    origin:["http://localhost:5173"],
+    method: ['GET','POST','PUT'],
+    credentials: true
+}));
+app.use(express.json());
+app.use("/auth",adminRouter);
+
 
 app.listen(port, () => {
     console.log("Server is running!")
--- Employee MS/readme.txt
+++ Employee MS/readme.txt
@@ -3,5 +3,7 @@ Following these steps to start the website:
 2.command: cd Server
 3.command: npm start
 4.Create second terminal
+5.command: cd employeems
+5.command: npm i vite
 5.command: npm run dev
-6.run localhost:1573/ on browser
+6.run localhost:5173/ on browser
